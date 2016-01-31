a33ea5cf00be2b47f758ef98bd44519e02432e92
DGivney <daniel@givneysoftware.com.au>
2016-01-31 11:42:50 +1100

    Update layout with new design & template.

--- assets/script.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    $('#compile').on('click', function(e) {
-        $('#code').buffalo({
-            debug: false,
-            maxRecursion: 5000
-        });
-    });
-});
